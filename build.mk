7bce546fc85938d7aea815326d0ad83611f8d9f3
tbreslein <tommy.breslein@protonmail.com>
2026-03-26 08:00:19 +0100

    totem works again

--- build.mk
+++ build.mk
@@ -1,11 +1,11 @@
-.PHONY: all
+.PHONY: all flash-totem
 
-all: ferris_sweep_tbreslein_rp2040_ce.uf2 totem_tbreslein.uf2
+all: ferris_sweep_tbreslein_rp2040_ce.uf2
 
 ferris_sweep_tbreslein_rp2040_ce.uf2: keyboards/ferris/keymaps/tbreslein/*
 	qmk lint -kb ferris/sweep -km tbreslein
 	qmk compile -c -j0 -kb ferris/sweep -km tbreslein -e CONVERT_TO=rp2040_ce
 
-totem_tbreslein.uf2: keyboards/totem/keymaps/tbreslein/*
-	# qmk lint -kb totem -km tbreslein
-	qmk compile -c -j0 -kb totem -km tbreslein
+flash-totem:
+	qmk flash -kb totem -km tbreslein -bl uf2-split-left
+	qmk flash -kb totem -km tbreslein -bl uf2-split-right
--- keyboards/totem/keymaps/tbreslein/rules.mk
+++ keyboards/totem/keymaps/tbreslein/rules.mk
@@ -1,6 +1,6 @@
 COMBO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
-# SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = yes
 EXTRAKEY_ENABLE = yes          # Audio control and System control
 KEY_OVERRIDE_ENABLE = yes
 MOUSEKEY_ENABLE = yes
