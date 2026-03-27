966f402bbfd08da53c7534730e82586f08a7c6c8
tbreslein <tommy.breslein@protonmail.com>
2026-03-27 08:07:20 +0100

    next totem iteration

--- keyboards/totem/keymaps/tbreslein/keymap.c
+++ keyboards/totem/keymaps/tbreslein/keymap.c
@@ -11,8 +11,9 @@ typedef enum {
     BASE = 0,
     GAME = 1,
     SYM = 2,
-    NUMNAV = 3,
-    FN = 4,
+    NUM = 3,
+    NAV = 4,
+    FN = 5,
 } _LAYERS;
 
 // #define MO_NAV TL_LOWR
@@ -32,24 +33,42 @@ typedef enum {
 typedef enum {
     QTB_GAME,
     YPN_BASE,
+    UI_TAB,
+    IO_STAB,
+    JK_ESC,
+    KL_ENT,
+    MCOMM_LBRC,
+    COMMDOT_RBRC,
 } _COMBOS;
 const uint16_t PROGMEM combo_qtb[] = {KC_Q, KC_T, KC_B, COMBO_END};
 const uint16_t PROGMEM combo_ypn[] = {KC_Y, KC_P, KC_N, COMBO_END};
+const uint16_t PROGMEM combo_ui[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_io[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_jk[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_kl[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_mcomm[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM combo_commdot[] = {KC_COMM, KC_DOT, COMBO_END};
 combo_t key_combos[] = {
     [QTB_GAME] = COMBO(combo_qtb, DF(GAME)),
     [YPN_BASE] = COMBO(combo_ypn, DF(BASE)),
+    [UI_TAB] = COMBO(combo_ui, KC_TAB),
+    [IO_STAB] = COMBO(combo_io, S(KC_TAB)),
+    [JK_ESC] = COMBO(combo_jk, KC_ESC),
+    [KL_ENT] = COMBO(combo_kl, KC_ENT),
+    [MCOMM_LBRC] = COMBO(combo_mcomm, KC_LBRC),
+    [COMMDOT_RBRC] = COMBO(combo_commdot, KC_RBRC),
 };
 
-const key_override_t ko_shift_comm_scln = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_SCLN);
-const key_override_t ko_shift_dot_colon = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COLN);
-const key_override_t ko_ctrl_comm_lt = ko_make_basic(MOD_MASK_ALT, KC_COMM, KC_LT);
-const key_override_t ko_ctrl_dot_gt = ko_make_basic(MOD_MASK_ALT, KC_DOT, KC_GT);
-const key_override_t *key_overrides[] = {
-    &ko_shift_comm_scln,
-    &ko_shift_dot_colon,
-    &ko_ctrl_comm_lt,
-    &ko_ctrl_dot_gt
-};
+// const key_override_t ko_shift_comm_scln = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_MINS);
+// const key_override_t ko_shift_dot_colon = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_UNDS);
+// const key_override_t ko_ctrl_comm_lt = ko_make_basic(MOD_MASK_ALT, KC_COMM, KC_LT);
+// const key_override_t ko_ctrl_dot_gt = ko_make_basic(MOD_MASK_ALT, KC_DOT, KC_GT);
+// const key_override_t *key_overrides[] = {
+//     &ko_shift_comm_scln,
+//     &ko_shift_dot_colon,
+//     &ko_ctrl_comm_lt,
+//     &ko_ctrl_dot_gt
+// };
 
 typedef enum {
     EMAIL = SAFE_RANGE,
@@ -67,41 +86,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 // TODO:
-// might not need a dedicated gaming layer?
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-                 KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-                 KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K,    KC_L,   KC_MINS,
-LALT_T(KC_LBRC), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LGUI_T(KC_RBRC),
-          LT(NUMNAV, KC_ESC), KC_SPC, KC_LSFT, /**/ LCTL_T(KC_BSPC), LT(SYM, KC_ENT), LT(FN, KC_TAB)
+                     KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,
+                     KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
+    LALT_T(KC_MINS), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LGUI_T(KC_QUOT),
+                 MO(NAV), MO(NUM), LSFT_T(KC_SPC), /**/ LCTL_T(KC_BSPC), MO(SYM), MO(FN)
     ),
 
     [GAME] = LAYOUT(
                  KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,
                  KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K,    KC_L,   KC_QUOT,
-LALT_T(KC_LBRC), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LGUI_T(KC_RBRC),
-          LT(NUMNAV, KC_ESC), KC_SPC, KC_LSFT, /**/ LCTL_T(KC_BSPC), LT(SYM, KC_ENT), LT(FN, KC_TAB)
+LALT_T(KC_MINS), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LGUI_T(KC_QUOT),
+             LT(NUM, KC_ESC), KC_LSFT, KC_SPC, /**/ LCTL_T(KC_BSPC), LT(SYM, KC_DQT), MO(FN)
     ),
 
     [SYM] = LAYOUT(
-         _______, KC_AT,   KC_LPRN, KC_LPRN, KC_BSLS, /**/ _______, KC_QUOT, KC_DQT,  _______, _______,
+         KC_AT,   KC_LCBR, KC_LBRC, KC_LPRN, KC_BSLS, /**/ _______, KC_RPRN, KC_RBRC, KC_RCBR, _______,
          KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_AMPR, /**/ _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
 KC_HASH, KC_CIRC, KC_PERC, KC_EXLM, KC_DLR,  KC_PIPE, /**/ _______, _______, KC_LT,   KC_GT,   KC_QUES, _______,
                            _______, KC_GRV,  KC_TILD, /**/ _______, _______, _______
     ),
 
-    [NUMNAV] = LAYOUT(
-         _______,  MS_LEFT,         MS_DOWN,         MS_UP,         MS_RGHT, /**/ MS_BTN2, KC_7, KC_8, KC_9, KC_PGUP,
-         KC_LGUI,  LALT_T(KC_LEFT), LSFT_T(KC_DOWN), LCTL_T(KC_UP), KC_RGHT, /**/ MS_BTN1, KC_4, KC_5, KC_6, KC_PGDN,
-_______, _______,  MS_WHLL,         MS_WHLD,         MS_WHLU,       MS_WHLR, /**/ MS_BTN3, KC_1, KC_2, KC_3, KC_HOME, KC_END,
-                                                  _______, _______, _______, /**/ KC_DEL, KC_0, _______
+    [NUM] = LAYOUT(
+         _______,  _______, KC_COMM, KC_DOT,  _______, /**/ KC_ASTR, KC_7, KC_8, KC_9, KC_PERC,
+         KC_LGUI,  KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_PLUS, KC_4, KC_5, KC_6, KC_EQL,
+_______, _______,  _______, _______, _______, _______, /**/ KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_CIRC,
+                            _______, _______, _______, /**/ KC_DEL, KC_0, _______
+    ),
+
+    [NAV] = LAYOUT(
+         _______, _______, MS_BTN4, MS_BTN5, _______, /**/ MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_PGUP,
+         KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
+_______, _______, _______, _______, _______, _______, /**/ MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_HOME, KC_END,
+                           _______, _______, _______, /**/ MS_BTN1, MS_BTN2, MS_BTN3
     ),
 
     [FN] = LAYOUT(
         KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F13, /**/ EMAIL,     KC_MPRV, KC_MPLY, KC_MNXT, KC_BRIU,
-        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ G(KC_TAB), KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
-KC_F16, KC_F9, KC_F10, KC_F11, KC_F12, KC_F15, /**/ _______,   KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, _______,
+        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
+KC_F16, KC_F9, KC_F10, KC_F11, KC_F12, KC_F15, /**/ _______,   KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, G(KC_TAB),
                     _______, KC_CAPS, CW_TOGG, /**/ _______, _______, _______
     ),
 };
--- keyboards/totem/keymaps/tbreslein/rules.mk
+++ keyboards/totem/keymaps/tbreslein/rules.mk
@@ -2,5 +2,5 @@ COMBO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 SPLIT_KEYBOARD = yes
 EXTRAKEY_ENABLE = yes          # Audio control and System control
-KEY_OVERRIDE_ENABLE = yes
+# KEY_OVERRIDE_ENABLE = yes
 MOUSEKEY_ENABLE = yes
