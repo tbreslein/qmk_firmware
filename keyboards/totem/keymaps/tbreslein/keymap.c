3425c68edf906b2c387232727b1ba17afa7c664e
tbreslein <tommy.breslein@protonmail.com>
2026-04-08 19:41:10 +0200

    give me back my combos

--- keyboards/totem/keymaps/tbreslein/keymap.c
+++ keyboards/totem/keymaps/tbreslein/keymap.c
@@ -30,16 +30,28 @@ typedef enum {
 typedef enum {
     QTB_GAME,
     YPN_BASE,
+    UI_TAB,
+    IO_STAB,
+    JK_ESC,
+    KL_ENT,
     MCOMM_LBRC,
     COMMDOT_RBRC,
 } _COMBOS;
 const uint16_t PROGMEM combo_qtb[] = {KC_Q, KC_T, KC_B, COMBO_END};
 const uint16_t PROGMEM combo_ypn[] = {KC_Y, KC_P, KC_N, COMBO_END};
+const uint16_t PROGMEM combo_ui[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_io[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_jk[] = {HM_J, HM_K, COMBO_END};
+const uint16_t PROGMEM combo_kl[] = {HM_K, HM_L, COMBO_END};
 const uint16_t PROGMEM combo_mcomm[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM combo_commdot[] = {KC_COMM, KC_DOT, COMBO_END};
 combo_t key_combos[] = {
     [QTB_GAME] = COMBO(combo_qtb, DF(GAME)),
     [YPN_BASE] = COMBO(combo_ypn, DF(BASE)),
+    [UI_TAB] = COMBO(combo_ui, KC_TAB),
+    [IO_STAB] = COMBO(combo_io, S(KC_TAB)),
+    [JK_ESC] = COMBO(combo_jk, KC_ESC),
+    [KL_ENT] = COMBO(combo_kl, KC_ENT),
     [MCOMM_LBRC] = COMBO(combo_mcomm, KC_LBRC),
     [COMMDOT_RBRC] = COMBO(combo_commdot, KC_RBRC),
 };
@@ -84,44 +96,34 @@ LT(NAV, S(KC_TAB)), LT(NUM, KC_ESC), KC_SPC, /**/ KC_BSPC, LT(SYM, KC_ENT), LT(F
     [GAME] = LAYOUT(
                  KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,
                  KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, HM_J, HM_K,    HM_L,   HM_SCLN,
-LSFT_T(KC_MINS), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
-       LT(FN, KC_ESC), LALT_T(KC_TAB), KC_SPC, /**/ KC_BSPC, LT(SYM, KC_ENT), LT(FN, KC_TAB)
+LALT_T(KC_MINS), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+             LT(NUM, KC_ESC), KC_LSFT, KC_SPC, /**/ KC_BSPC, MO(SYM), MO(FN)
     ),
 
-    // IDEA: this could become a layer that is accessible through a combo on the
-    // GAME layer with a one-shot. then the keys could become things that
-    // wouldn't be accessible otherwise, like numbers and symbols
-//     [GAMEEXTRA] = LAYOUT(
-//         KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F13, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-//         KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ KC_H, HM_J, HM_K,    HM_L,   HM_SCLN,
-// KC_F16, KC_F9, KC_F10, KC_F11, KC_F12, KC_F15, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
-//                     _______, _______, _______, /**/ KC_BSPC, LT(SYM, KC_ENT), LT(FN, KC_TAB)
-//     ),
-
     [SYM] = LAYOUT(
-         _______, KC_AT,   KC_LPRN, KC_RPRN, KC_BSLS, /**/ _______, KC_TAB,  S(KC_TAB), _______, _______,
-         KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_AMPR, /**/ _______, KC_RSFT, KC_RCTL,   KC_LALT, KC_RGUI,
-KC_HASH, KC_CIRC, KC_PERC, KC_EXLM, KC_DLR,  KC_PIPE, /**/ _______, _______, KC_LT,     KC_GT,   KC_QUES, _______,
+         _______, KC_AT,   KC_LPRN, KC_RPRN, KC_BSLS, /**/ _______, KC_LPRN, KC_RPRN, _______, _______,
+         KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_AMPR, /**/ _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
+KC_HASH, KC_CIRC, KC_PERC, KC_EXLM, KC_DLR,  KC_PIPE, /**/ _______, _______, KC_LT,   KC_GT,   KC_QUES, _______,
                            _______, KC_GRV,  KC_TILD, /**/ _______, _______, _______
     ),
 
     [NUM] = LAYOUT(
          _______,  _______, KC_COMM, KC_DOT,  _______, /**/ KC_ASTR, KC_7, KC_8, KC_9, KC_PERC,
-         KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, _______, /**/ KC_PLUS, KC_4, KC_5, KC_6, KC_EQL,
+         KC_LGUI,  KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_PLUS, KC_4, KC_5, KC_6, KC_EQL,
 _______, _______,  _______, _______, _______, _______, /**/ KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_CIRC,
                             _______, _______, _______, /**/ KC_DEL, KC_0, _______
     ),
 
     [NAV] = LAYOUT(
          _______, _______, MS_BTN4, MS_BTN5, _______, /**/ MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_PGUP,
-         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
+         KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
 _______, _______, _______, _______, _______, _______, /**/ MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_HOME, KC_END,
                            _______, _______, _______, /**/ MS_BTN1, MS_BTN2, MS_BTN3
     ),
 
     [FN] = LAYOUT(
         KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F13, /**/ EMAIL,   KC_MPRV, KC_MPLY, KC_MNXT, KC_BRIU,
-        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
+        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
 KC_F16, KC_F9, KC_F10, KC_F11, KC_F12, KC_F15, /**/ _______, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, G(KC_TAB),
                     _______, KC_CAPS, CW_TOGG, /**/ _______, _______, _______
     ),
