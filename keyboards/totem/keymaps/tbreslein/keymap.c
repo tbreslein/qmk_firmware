e11499172e906facf57cf302101c44ccec28f76f
tbreslein <tommy.breslein@protonmail.com>
2026-04-08 12:45:01 +0200

    switch control and shift home row mods

--- keyboards/totem/keymaps/tbreslein/keymap.c
+++ keyboards/totem/keymaps/tbreslein/keymap.c
@@ -19,11 +19,11 @@ typedef enum {
 
 #define HM_A LGUI_T(KC_A)
 #define HM_S LALT_T(KC_S)
-#define HM_D LSFT_T(KC_D)
-#define HM_F LCTL_T(KC_F)
+#define HM_D LCTL_T(KC_D)
+#define HM_F LSFT_T(KC_F)
 
-#define HM_J RCTL_T(KC_J)
-#define HM_K RSFT_T(KC_K)
+#define HM_J RSFT_T(KC_J)
+#define HM_K RCTL_T(KC_K)
 #define HM_L LALT_T(KC_L)
 #define HM_SCLN RGUI_T(KC_SCLN)
 
@@ -100,28 +100,28 @@ LSFT_T(KC_MINS), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT,
 
     [SYM] = LAYOUT(
          _______, KC_AT,   KC_LPRN, KC_RPRN, KC_BSLS, /**/ _______, KC_TAB,  S(KC_TAB), _______, _______,
-         KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_AMPR, /**/ _______, KC_RCTL, KC_RSFT,   KC_LALT, KC_RGUI,
+         KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_AMPR, /**/ _______, KC_RSFT, KC_RCTL,   KC_LALT, KC_RGUI,
 KC_HASH, KC_CIRC, KC_PERC, KC_EXLM, KC_DLR,  KC_PIPE, /**/ _______, _______, KC_LT,     KC_GT,   KC_QUES, _______,
                            _______, KC_GRV,  KC_TILD, /**/ _______, _______, _______
     ),
 
     [NUM] = LAYOUT(
          _______,  _______, KC_COMM, KC_DOT,  _______, /**/ KC_ASTR, KC_7, KC_8, KC_9, KC_PERC,
-         KC_LGUI,  KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_PLUS, KC_4, KC_5, KC_6, KC_EQL,
+         KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, _______, /**/ KC_PLUS, KC_4, KC_5, KC_6, KC_EQL,
 _______, _______,  _______, _______, _______, _______, /**/ KC_MINS, KC_1, KC_2, KC_3, KC_SLSH, KC_CIRC,
                             _______, _______, _______, /**/ KC_DEL, KC_0, _______
     ),
 
     [NAV] = LAYOUT(
          _______, _______, MS_BTN4, MS_BTN5, _______, /**/ MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_PGUP,
-         KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
+         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
 _______, _______, _______, _______, _______, _______, /**/ MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_HOME, KC_END,
                            _______, _______, _______, /**/ MS_BTN1, MS_BTN2, MS_BTN3
     ),
 
     [FN] = LAYOUT(
         KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F13, /**/ EMAIL,   KC_MPRV, KC_MPLY, KC_MNXT, KC_BRIU,
-        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
+        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F14, /**/ _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
 KC_F16, KC_F9, KC_F10, KC_F11, KC_F12, KC_F15, /**/ _______, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, G(KC_TAB),
                     _______, KC_CAPS, CW_TOGG, /**/ _______, _______, _______
     ),
